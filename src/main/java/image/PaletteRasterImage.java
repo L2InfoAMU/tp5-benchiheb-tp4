0c6520826f5a2d15e117dbf95122fcaf15f8a476
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-02 21:33:34 +0100

    Add files via upload
    constructor done

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,15 @@
+package image;
+
+import java.awt.*;
+
+public class PaletteRasterImage implements Image {
+    private Color color;
+    private int width;
+    private int height;
+
+    public PaletteRasterImage(Color color, int width, int height){
+        this.width=width;
+        this.height=height;
+        this.color=color;
+    }
+}
