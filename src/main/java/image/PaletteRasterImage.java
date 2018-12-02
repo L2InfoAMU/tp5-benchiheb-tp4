5db7e920a7840a47831ccfdbffc622e2a4322e62
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-02 21:46:34 +0100

    Add files via upload
    getPixelColor(x,y) and setPixelsColor(pixels[][]) done

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -7,13 +7,13 @@ public class PaletteRasterImage implements Image {
     private int width;
     private int height;
 
-    public PaletteRasterImage(Color color, int width, int height){
-        this.width=width;
-        this.height=height;
-        this.color=color;
+    public PaletteRasterImage(Color color, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
-    public void createRepresentation(){
+    public void createRepresentation() {
         int[][] window = new int[width][height];
     }
 
@@ -22,16 +22,30 @@ public class PaletteRasterImage implements Image {
     }
 
 
-    public int getWidth(){
+    public int getWidth() {
         return width;
     }
-    protected void setHeight(int height){
+
+    protected void setHeight(int height) {
 
         this.height = height;
     }
 
-    protected void setWidth(int width){
+    protected void setWidth(int width) {
 
         this.width = width;
+    }
+
+    public Color getPixelColor(int x, int y) {
+        final Color pixelColor = getPixelColor (x, y);
+        return pixelColor;
+
+    }
+
+    public void setPixelsColor(Color[][] pixels){
+        for(int i=0; i<width ;i++)
+            for (int j = 0; j < height; j++) {
+                getPixelColor(i,j)=pixels[i][j];
+            }
     }
 }
