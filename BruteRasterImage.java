6f45f5759eaafe524fb723c910f736ddaa97d89b
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-02 20:08:26 +0100

    Add files via upload
    createrepresentation done

---
+++ BruteRasterImage.java
@@ -0,0 +1,49 @@
+package image;
+
+import java.awt.*;
+
+public class BruteRasterImage implements Image {
+    public int height;
+    public  int width;
+    public Color color;
+
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getWidth(){
+        return width;
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        final Color pixelColor = getPixelColor (x, y);
+        return pixelColor;
+    }
+
+    protected void setHeight(int height){
+
+        this.height = height;
+    }
+
+    protected void setWidth(int width){
+
+        this.width = width;
+    }
+
+    public void createRepresentation(){
+
+        int[][] window = new int[width][height];
+    }
+
+
+
+
+
+
+
+
+}
