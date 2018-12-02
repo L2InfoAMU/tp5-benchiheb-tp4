519c06a900a0f0760c1acacda4520e2bb0cebb26
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-02 21:29:17 +0100

    Add files via upload
    final upload

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,18 +1,17 @@
 package image;
 
+
 import java.awt.*;
 
 public class BruteRasterImage implements Image {
-    public int height;
-    public  int width;
-    public Color color;
+    private int height;
+    private   int width;
 
 
     public BruteRasterImage(Color color, int width, int height){
 
         this.width=width;
         this.height=height;
-        this.color=color;
     }
 
 
@@ -21,15 +20,16 @@ public class BruteRasterImage implements Image {
 
     public void setPixelColor(Color color, int x, int y){
         final Color pixelColor = getPixelColor(x, y);
+
     }
 
 
     private void setPixelsColor(Color[][] pixels){
-       Color getPixelColor(int x,int y);
+
         for(int i=0; i<width ;i++)
             for (int j = 0; j < height; j++) {
 
-              pixels[i][j]= getPixelColor(x,y);
+              pixels[i][j]= getPixelColor(i,j);
             }
     }
 
@@ -41,17 +41,16 @@ public class BruteRasterImage implements Image {
             }
         }
     }
-    @Override
+
     public int getHeight() {
         return height;
     }
 
-    @Override
+
     public int getWidth(){
         return width;
     }
 
-    @Override
     public Color getPixelColor(int x, int y) {
         final Color pixelColor = getPixelColor (x, y);
         return pixelColor;
