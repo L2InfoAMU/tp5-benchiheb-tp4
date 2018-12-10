f119859d9eb72687349519948b16e0961db21519
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-10 12:17:52 +0100

    Add files via upload
    setPixelColor(Color color, int x, int y)
    getPixelColor(int x, int y) and setPixelsColor(Color[][] pixels) done

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -15,16 +15,23 @@ public class SparseRasterImage extends RasterImage{
         this.color = color;
     }
 
+    public void setPixelColor(Color color, int x, int y){
+        this.setPixelColor(color,x,y);
+    }
+    public Color getPixelColor(int x, int y){
+        return getPixelColor(x,y);
+    }
+    private void setPixelsColor(Color[][] pixels){
+        for(int i=0;i<widht;i++){
+            for(int j=0;j<height;j++){
+                pixels[i][j]=getPixelColor(i,j);
+            }
+        }
+    }
 
     public int getWidht() {
         return widht;
     }
-
-    public void setWidht(int widht) {
-        this.widht = widht;
-    }
-
-
 
     public int getHeight(){
         return height;
