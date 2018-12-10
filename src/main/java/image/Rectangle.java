318d128b673b42af55e4e9da7665fb67cf2dcf0c
ahmedben96 <43950840+ahmedben96@users.noreply.github.com>
2018-12-10 19:21:44 +0100

    Add files via upload
    Rectangle constructor done

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,20 @@
+package image;
+
+import java.awt.*;
+
+public class Rectangle implements Shape {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Color color;
+
+    public Rectangle(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        new Rectangle(x,y,width,height,color);
+    }
+}
